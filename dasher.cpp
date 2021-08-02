b32d6c33cc649077b5fab513b605abfc159fe6c6
Daryl Jacura <d.jacura@sky.com>
2021-08-02 19:04:27 +0100

    Added sprite texture and sheet

--- dasher.cpp
+++ dasher.cpp
@@ -9,6 +9,27 @@ int main()
     // Initilize the window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+    // acceleration due to gravity (pixels per frame) per frame (p/f)/f
+    const int gravity{1};
+
+    // is rectangle in the air?
+    bool isInAir{};
+
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+    scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+    scarfyPos.y = windowHeight - scarfyRec.height;
+
+    // jump velocity
+    const int jumpVel{-22};
+
+    int velocity{0};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -16,8 +37,33 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        // ground check
+        if (scarfyPos.y >= windowHeight - scarfyRec.height)
+        {
+            // rectangle is on the ground
+            velocity = 0;
+            isInAir = false;
+        }
+        else
+        {
+            // rectangle is in the air
+            velocity += gravity;
+            isInAir = true;
+        }
+
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocity += jumpVel;
+        }
+
+        // update position
+        scarfyPos.y += velocity;
+
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
         // Stop drawing
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 }
